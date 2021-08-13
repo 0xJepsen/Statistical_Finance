51256adf7cbdd950a06ce2a95a10d63175ad3d0e
Waylon Jepsen <57912727+0xJepsen@users.noreply.github.com>
2021-08-13 12:27:33 -0600

    Update script.py

--- script.py
+++ script.py
@@ -1,27 +1,35 @@
-from utils import calculate_variance
-import numpy as np
-from math import sqrt
-
+from utils import *
 
 def display_as_percentage(val):
-    return '{:.1f}%'.format(val * 100)
+  return '{:.1f}%'.format(val * 100)
 
-returns_disney = [0.22, 0.12, 0.01, 0.05, 0.04]
-returns_cbs = [-0.13, -0.15, 0.31, -0.06, -0.29]
-
-stddev_disney = np.std(returns_disney)
-stddev_cbs = np.std(returns_cbs)
+amazon_prices = [1699.8, 1777.44, 2012.71, 2003.0, 1598.01, 1690.17, 1501.97, 1718.73, 1639.83, 1780.75, 1926.52, 1775.07, 1893.63]
+ebay_prices = [35.98, 33.2, 34.35, 32.77, 28.81, 29.62, 27.86, 33.39, 37.01, 37.0, 38.6, 35.93, 39.5]
 
 # Write code here
-def calculate_stddev(dataset):
-    variance = calculate_variance(dataset)
-    stddev = sqrt(variance)
-    return stddev
+def get_returns(prices):
+  returns = []
+  for i in range(len(prices)-1):
+    start_price = prices[i]
+    end_price = prices[i+1]
+    returns.append(calculate_log_return(start_price, end_price))
+  return returns
 
-stddev_disney = calculate_stddev(returns_disney)
-stddev_cbs = calculate_stddev(returns_cbs)
+amazon_returns = get_returns(amazon_prices)
+ebay_returns = get_returns(ebay_prices)
 
+anual_amazon_return = display_as_percentage(sum(amazon_returns))
+anual_ebay_return = display_as_percentage(sum(ebay_returns))
+print(anual_amazon_return)
+print(anual_ebay_return)
 
+amz_var = calculate_variance(amazon_returns)
+ebay_var = calculate_variance(ebay_returns)
 
-print('The standard deviation of Disney stock returns is', display_as_percentage(stddev_disney))
-print('The standard deviation of CBS stock returns is', display_as_percentage(stddev_cbs))
+amz_stddev = calculate_stddev(amazon_returns)
+ebay_stddev = calculate_stddev(ebay_returns)
+
+print(display_as_percentage(amz_stddev))
+print(display_as_percentage(ebay_stddev))
+
+print(calculate_correlation(amazon_returns, ebay_returns))
